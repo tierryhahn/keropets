df8857faada3ed1bfa6548d5f0cf0881498d1a8c
ThiagoDalto <56813158+ThiagoDalto@users.noreply.github.com>
2022-11-01 15:28:57 -0300

    Feat:=> Criação da rota de Login de ong

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import { appRoutes } from './routes'
 import { errorMiddleware } from './middlewares/error.middleware'
 import { Request, Response } from 'express'
 import ongRoutes from './routes/ong/routes'
+import sessionRoutes from './routes/ong/session/routes'
     
 const app = express()
     
 app.use(express.json())
 app.use("/ong", ongRoutes)
+app.use("/ong", sessionRoutes)
     
 appRoutes(app)
     
