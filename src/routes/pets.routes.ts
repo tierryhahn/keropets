39e1327756c4520e06f8c4b3c3e8a70daa6c5e19
Rafael <rafaelskz92@gmail.com>
2022-11-03 13:53:56 -0300

    feat: Create route delete pet

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express'
 
 import listPetsController from '../controllers/pets/listPets.controller'
 import updatePetController from '../controllers/pets/updatePet.controller'
+import deletePetController from '../controllers/pets/deletePet.controller'
 
 const petRoutes = Router()
 
 petRoutes.get('', listPetsController)
 petRoutes.patch('/:id', updatePetController)
+petRoutes.delete('/:id', deletePetController)
 
 export default petRoutes
