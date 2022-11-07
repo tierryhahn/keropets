87498c97541783c33327e10767b9a56f0e0a5f54
Tierry <tierryhahn@gmail.com>
2022-11-07 14:16:40 -0300

    att/deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,22 +1,29 @@
 import { DataSource } from "typeorm";
-import "dotenv/config"
-    
-    export const AppDataSource = new DataSource({
-        type: "postgres",
-        url: process.env.DATABASE_URL,
-        ssl: process.env.NODE_END === "production" ?
-            {rejectUnauthorized: false}
-            : false,
-        synchronize: false,
-        logging: true,
-        entities: process.env.NODE_ENV === "production" ? ["dist/entities/*.js"] : ["src/entities/*.ts"],
-        migrations: process.env.NODE_ENV === "production" ? ["dist/migrations/*.js"] : ["src/migrations/*.ts"],
-    })
-    
-    AppDataSource.initialize()
-        .then(() => {
-            console.log("Data Source initialized")
-        })
-        .catch((err) => {
-            console.error("Error during Data Source initialization", err)
-        })       
+import "dotenv/config";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? {ca: process.env.SSL_CERT}
+      : false,
+  synchronize: false,
+  logging: true,
+  entities:
+    process.env.NODE_ENV === "production"
+      ? ["dist/entities/*.js"]
+      : ["src/entities/*.ts"],
+  migrations:
+    process.env.NODE_ENV === "production"
+      ? ["dist/migrations/*.js"]
+      : ["src/migrations/*.ts"],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source initialized");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+  });
