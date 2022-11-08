15aacfc3804b7fcc1a12b62b4970d973bd02fd9a
ThiagoDalto <56813158+ThiagoDalto@users.noreply.github.com>
2022-11-08 16:44:33 -0300

    Criação do teste de login das ong

--- src/routes/ong/routes.ts
+++ src/routes/ong/routes.ts
@@ -5,6 +5,7 @@ import ongDeleteController from "../../controllers/ong/ongDelete.controller";
 import ongUpdateController from "../../controllers/ong/ongUpdate.controller";
 import ongListController from "../../controllers/ong/ongList.controller";
 import ongListByIdController from "../../controllers/ong/ongListById.controller";
+import ensureOngAuthMiddleware from "../../middlewares/authOng.middleware";
 
 
 const ongRoutes = Router();
@@ -12,14 +13,14 @@ ongRoutes.post(
     "",
     ongCreateController
 )
-ongRoutes.patch("/:id", ongUpdateController)
-ongRoutes.delete("/:id", ongDeleteController)
+ongRoutes.patch("/:id", ensureOngAuthMiddleware, ongUpdateController)
+ongRoutes.delete("/:id", ensureOngAuthMiddleware, ongDeleteController)
 ongRoutes.get(
-    "",
+    "", ensureOngAuthMiddleware,
     ongListController
 )
 ongRoutes.get(
-    "/:id",
+    "/:id", ensureOngAuthMiddleware,
     ongListByIdController
 )
 
