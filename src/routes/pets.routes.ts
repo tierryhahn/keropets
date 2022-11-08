7a7794f3ffb96e8262c72879dcf1fc5b5db83a08
Tierry <tierryhahn@gmail.com>
2022-11-08 15:11:37 -0300

    fix/listPet&UserID

--- src/routes/pets.routes.ts
+++ src/routes/pets.routes.ts
@@ -1,15 +1,17 @@
-import { Router } from 'express'
+import { Router } from "express";
 
-import petCreatecontroller from "../controllers/pet/petCreate.controller";
-import listPetsController from '../controllers/pets/listPets.controller'
-import updatePetController from '../controllers/pets/updatePet.controller'
-import deletePetController from '../controllers/pets/deletePet.controller'
+import petCreatecontroller from "../controllers/pets/petCreate.controller";
+import listPetsController from "../controllers/pets/listPets.controller";
+import updatePetController from "../controllers/pets/updatePet.controller";
+import deletePetController from "../controllers/pets/deletePet.controller";
+import listPetByIDController from "../controllers/pets/listPetByID.controller";
 
-const petRoutes = Router()
+const petRoutes = Router();
 
-petRoutes.post("",petCreatecontroller)
-petRoutes.get('', listPetsController)
-petRoutes.patch('/:id', updatePetController)
-petRoutes.delete('/:id', deletePetController)
+petRoutes.post("", petCreatecontroller);
+petRoutes.get("", listPetsController);
+petRoutes.get("/:id", listPetByIDController)
+petRoutes.patch("/:id", updatePetController);
+petRoutes.delete("/:id", deletePetController);
 
-export default petRoutes
+export default petRoutes;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,14 +2,16 @@ import { Router } from "express";
 import { createUserController } from "../controllers/user/createUsers.controller";
 import { updateUserController } from "../controllers/user/updateUser.controller";
 
-import userLoginController from "../controllers/userLogin.controller";
+import userLoginController from "../controllers/user/userLogin.controller";
 import listUserController from "../services/user/listUsers.service";
+import listUserByIDController from "../controllers/user/listUserByID.controller";
 
 const userRoutes = Router();
 
-  userRoutes.post("/", createUserController)
-  userRoutes.post("/", userLoginController);
-  userRoutes.get("/", listUserController);
-  userRoutes.patch("", updateUserController);
+userRoutes.post("/", createUserController);
+userRoutes.post("/", userLoginController);
+userRoutes.get("/", listUserController);
+userRoutes.get("/:id", listUserByIDController)
+userRoutes.patch("", updateUserController);
 
-  export default userRoutes
+export default userRoutes;
